367aa79f1a27fb55614f569433a46d6001fb465a
shreyashlaxkar <laxkarshreyash@gmail.com>
2026-08-18 21:38:34 +0530

    Create age.java

---
+++ day_01/age.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class age{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your age:");
+        int age = sc.nextInt();
+        System.out.println("Your age is:" +age);
+        sc.close();
+    }
+}
