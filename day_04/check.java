66fbb36329965d3b84a6733787dd95e59b158a13
shreyashlaxkar <laxkarshreyash@gmail.com>
2026-08-22 10:42:52 +0530

    Create check.java

---
+++ day_04/check.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class check{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+
+        if(num>0){
+            System.out.println("The number is positive");
+        }
+        else if(num<0){
+            System.out.println("The number is negative");
+        }
+        else{
+            System.out.println("The number is zero");
+        }
+        sc.close();
+    }
+}
